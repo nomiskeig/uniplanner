ecf934f6f72eaa411e2698c0e64c972385dd6bd5
Nomiskeig <nomiskeig@gmail.com>
2024-06-20 19:44:28 +0200

    Move frontend to frontend directory

---
+++ uniplanner/src/main/java/de/giek/uniplanner/UniplannerApplication.java
@@ -0,0 +1,13 @@
+package de.giek.uniplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniplannerApplication.class, args);
+	}
+
+}
