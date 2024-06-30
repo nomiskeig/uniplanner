42f7255ce1fb39e0c9a4bc1c130de3a39f42dcf4
Nomiskeig <nomiskeig@gmail.com>
2024-06-30 17:29:03 +0200

    Frontend: Add a button component

---
+++ frontend/components/Button.tsx
@@ -0,0 +1,11 @@
+export interface ButtonProps {
+    onClick:  () => void,
+    text: string
+}
+
+
+export function Button(props: ButtonProps) {
+    return <button onClick={() => props.onClick()}>{props.text}</button>
+
+
+}
