f6715d530293e0584752bdaa186fef05e6a10be2
Nomiskeig <nomiskeig@gmail.com>
2024-06-30 17:30:04 +0200

    Frontend: Add the login page

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,49 @@
+"use client"
+
+import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
+import { UserContext } from "@/components/UserContext";
+import React, { useContext } from "react";
+
+export default function Page() {
+    const [usernameInput, setUsernameInput] = React.useState<string>("");
+    const [passwordInput, setPasswordInput] = React.useState<string>("");
+    const userContext = useContext(UserContext)
+    function handleLogin() {
+        fetch("http://localhost:8080/api/v1/users/login", {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            mode: 'cors',
+            method: 'POST', body: JSON.stringify(
+                {
+                    "password": passwordInput,
+                    "username": usernameInput
+                }
+            )
+        })
+            .then(res => {
+                if (!res.ok) {
+                    alert("Error when logging in: " +  res.status)
+
+                }
+
+                return res.json()
+            })
+            .then(data => { userContext.setUser({ token: data.token, username: usernameInput, isLoggedIn: true }) })
+            .catch(err => console.log(err.message))
+
+
+    }
+
+    return <div>
+        <div>
+            Login
+            <Input title={"Username"} type={"text"} value={usernameInput} onChange={(newValue: string) => setUsernameInput(newValue)}></Input>
+            <Input title={"Password"} type={"password"} value={passwordInput} onChange={(newValue: string) => setPasswordInput(newValue)}></Input>
+            <Button onClick={handleLogin} text={"Log in"}></Button>
+        </div>
+
+    </div>
+}
+
