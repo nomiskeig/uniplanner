11945caef516cfc07c05107bc8dc6eee30d1e09d
Nomiskeig <nomiskeig@gmail.com>
2024-07-02 22:35:44 +0200

    Frontend: Improve design of the ButtonMenu component

--- frontend/components/ButtonMenu.tsx
+++ frontend/components/ButtonMenu.tsx
@@ -1,3 +1,5 @@
+import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { useState } from "react"
 export interface ButtonMenuOption {
     text: string,
@@ -10,13 +12,15 @@ export interface ButtonMenuProps {
 }
 export function ButtonMenu(props: ButtonMenuProps) {
     let [expanded, setExpanded] = useState(false)
-
+    if (props.options.length == 0) {
+        props.options.push({text: "No options available.", action: () =>{}})
+    }
 
     return <div>
-        <div onClick={() => setExpanded(true)}>Options</div>
-        {expanded ? <div>
+        <div className="cursor-pointer" onClick={() => {console.log("clicked"); setExpanded(!expanded);}}><FontAwesomeIcon icon={faEllipsisVertical} /></div>
+        {expanded ? <div className="absolute right-10 border-2 border-black rounded">
             {props.options.map((option, index) => {
-                return <div className="cursor-pointer bg-gray-400 " key={index} onClick={() => option.action()}>{option.text}</div>
+                return <div className="cursor-pointer bg-gray-200 hover:bg-gray-300 right-10 p-1" key={index} onClick={() => option.action()}>{option.text}</div>
             })}
             
 
