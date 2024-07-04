a48a265f5bd60e2cff76f4b5f1290c4d918c51e3
Nomiskeig <nomiskeig@gmail.com>
2024-07-04 23:16:05 +0200

    Frontend: Improve the ButtonMenu component

--- frontend/components/ButtonMenu.tsx
+++ frontend/components/ButtonMenu.tsx
@@ -14,22 +14,26 @@ export interface ButtonMenuProps {
 export function ButtonMenu(props: ButtonMenuProps) {
     let [expanded, setExpanded] = useState(false)
     if (props.options.length == 0) {
-        props.options.push({text: "No options available.", action: () =>{}})
+        props.options.push({ text: "No options available.", action: () => { } })
     }
-    let ref= useOutsideClick(() => {
+    let ref = useOutsideClick(() => {
         setExpanded(false)
     })
+    function handleClick(option: ButtonMenuOption) {
+        option.action();
+        setExpanded(false);
+    }
 
     return <div>
-        <div className="cursor-pointer" onClick={() => {console.log("clicked"); setExpanded(!expanded);}}><FontAwesomeIcon icon={faEllipsisVertical} /></div>
+        <div className="cursor-pointer" onClick={() => setExpanded(!expanded)}><FontAwesomeIcon icon={faEllipsisVertical} /></div>
         {expanded ? <div ref={ref} className="absolute right-10 border-2 border-black rounded">
             {props.options.map((option, index) => {
-                return <div className="cursor-pointer bg-gray-200 hover:bg-gray-300 right-10 p-1" key={index} onClick={() => option.action()}>{option.text}</div>
+                return <div className="cursor-pointer bg-gray-200 hover:bg-gray-300 right-10 p-1" key={index} onClick={() => handleClick(option)}>{option.text}</div>
             })}
-            
 
-        </div> : null} 
 
-        
+        </div> : null}
+
+
     </div>
 }
