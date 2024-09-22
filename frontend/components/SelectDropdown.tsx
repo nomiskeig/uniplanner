7a8b9e535f5b7d6c757dedd9f3c0ea8cd7d95076
Simon Giek <nomiskeig@gmail.com>
2024-09-22 14:29:49 +0200

    Highlight the current selected element in selectdropdown

--- frontend/components/SelectDropdown.tsx
+++ frontend/components/SelectDropdown.tsx
@@ -11,13 +11,13 @@ export interface DropdownProps<T> {
 export interface DropdownOption<T> {
     name: string,
     element: T
-    callback: (option: T) => void | boolean 
+    callback: (option: T) => void | boolean
 
 }
 
 export function SelectDropdown<T>(props: DropdownProps<T>) {
     let [expanded, setExpanded] = React.useState(false);
-    let [current, setCurrent] = React.useState<DropdownOption<T>| null>(props.options[props.defaultIndex]);
+    let [current, setCurrent] = React.useState<DropdownOption<T> | null>(props.options[props.defaultIndex]);
     let ref = useOutsideClick(() => { setExpanded(false) });
     if (current == null) {
         return;
@@ -28,8 +28,8 @@ export function SelectDropdown<T>(props: DropdownProps<T>) {
             <div
                 className={`w-full border border-black p-2 rounded cursor-pointer bg-gray-300 ${expanded ? "after:content-['\\25B2']" : "after:content-['\\25BC']"} after:float-right`}
                 onClick={() => { setExpanded(!expanded) }}>{current.name}</div>
-            {expanded ? <div ref={ref}className="absolute border-black border-2 mt-2 rounded">{props.options.map((option, index) => {
-                return <div className="cursor-pointer py-1 px-2 hover:bg-gray-300 border-black bg-gray-200"
+            {expanded ? <div ref={ref} className="absolute border-black border-2 mt-2 rounded">{props.options.map((option, index) => {
+                return <div className={`cursor-pointer py-1 px-2 hover:bg-gray-300 border-black ${current.name == option.name ? "bg-gray-400" : "bg-gray-200"}`}
                     onClick={() => {
                         option.callback(option.element);
                         setCurrent(option);
